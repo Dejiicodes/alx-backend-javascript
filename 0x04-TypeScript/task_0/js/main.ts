fde838b527936c4322fa7f37633b6320aeade461
Dejiicodes <adewuniayodeji17@gmail.com>
2024-07-31 13:33:14 +0000

    Initial commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Ayo',
+    lastName: 'Kunle',
+    age: 34,
+    location: 'Ogba',
+}
+
+const student2: Student = {
+    firstName: 'Kunle',
+    lastName: 'Ayo',
+    age: 43,
+    location: 'Ikeja',
+}
+
+let studentsList: Student[] = [];
+
+studentsList.push(student1);
+studentsList.push(student2);
+
+let myVar = document.createElement('table');
+
+for (let i = 0; i < studentsList.length; i++) {
+    let row = myVar.insertRow();
+    let name = row.insertCell();
+    let location = row.insertCell();
+    name.innerHTML = studentsList[i].firstName;
+    location.innerHTML = studentsList[i].location;
+}
+
+document.body.appendChild(myVar);
